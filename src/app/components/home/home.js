282021399b3c69f4c923e62fc1a5637c5e03304d
Anshuman <verma77712@gmail.com>
2024-03-24 00:14:58 +0530

    first commit

---
+++ src/app/components/home/home.js
@@ -0,0 +1,15 @@
+"use client";
+import React from 'react';
+import Navbar from '../navbar/navbar';
+import Body from '../body/body';
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Body/>
+    </div>
+  )
+}
+
+export default Home;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { StickyNavbar } from "./components/navbar/navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
