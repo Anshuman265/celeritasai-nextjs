daf13bbcdcb123e213af7a3a7e7a01608aeed18f
Anshuman <verma77712@gmail.com>
2024-03-30 15:03:51 +0530

    Changes

--- src/app/components/body/header.js
+++ src/app/components/body/header.js
@@ -169,8 +169,11 @@ export default function Header() {
               >
                 Discover More
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Contact Us <span aria-hidden="true">→</span>
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                View Demo
               </a>
             </div>
           </div>
